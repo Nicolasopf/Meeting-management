14467df058793a805396ec2f00193b0d9bf902ad
Nicolas <n.ico15935746@gmail.com>
2021-07-26 23:25:09 -0500

    [Added] Secret_key as environment

--- acme_inc/settings.py
+++ acme_inc/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^($vk!^uxeptt(e$3_^84ws!650twkpf8brc)*8k11h9!)o8bj'
+
+env_key = os.getenv("DJANGO_KEY")
+if not env_key:
+    print("Please use DJANGO_KEY=random_key as environment variable")
+    exit()
+SECRET_KEY = 'django-insecure-' + env_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
