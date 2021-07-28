87fb3e5031911a337f00800de956bc06838314de
Nicolas <n.ico15935746@gmail.com>
2021-07-28 00:49:39 -0500

    [Added] Registers to modify the API models

--- meetmanage/admin.py
+++ meetmanage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import Room, Reservation, Catering
 
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Reservation)
+admin.site.register(Catering)
