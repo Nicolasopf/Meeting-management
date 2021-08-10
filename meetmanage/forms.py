9a6b289e1ceb232bb61d4a834d580129883a2e84
Nicolas <n.ico15935746@gmail.com>
2021-08-10 03:37:15 -0500

    [Added] Form for login and signup

--- meetmanage/forms.py
+++ meetmanage/forms.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ''' Allow forms from django '''
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -10,14 +12,24 @@ class LoginForm(forms.Form):
         attrs={'class': 'fadeIn second', 'placeholder': 'Password'}))
 
 
-class SignUpForm(forms.Form):
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
-        attrs={'class': 'fadeIn second', 'placeholder': 'First name'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
-        attrs={'class': 'fadeIn second', 'placeholder': 'Last name'}))
-    username = forms.CharField(label="", max_length=50, widget=forms.TextInput(
-        attrs={'class': 'fadeIn second', 'placeholder': 'Username'}))
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(label="", max_length=100, widget=forms.TextInput(
         attrs={'class': 'fadeIn second', 'placeholder': 'Email Address'}))
-    password = forms.CharField(label="", min_length=8, widget=forms.PasswordInput(
-        attrs={'class': 'fadeIn second', 'placeholder': 'Password'}))
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username", "email"]
+        widgets = {'first_name': forms.TextInput(attrs={'type': 'text', 'class': 'fadeIn second', 'placeholder': 'First name'}),
+                   'last_name': forms.TextInput(attrs={'class': 'fadeIn second', 'placeholder': 'Last name'}),
+                   'username': forms.TextInput(attrs={'type': 'text', 'class': 'fadeIn second', 'placeholder': 'Username'}),
+                   'email': forms.TextInput(attrs={'type': 'email', 'class': 'fadeIn second', 'placeholder': 'Email'}),}
+
+#                   'password': forms.TextInput(attrs={'type': 'pasword', 'class': 'fadeIn second', 'placeholder': 'Password'}),}
+        help_texts = {'username': "", 'password2': ""}
+
+        labels = {'first_name': "",
+                  'last_name': "",
+                  'username': "",
+                  'email': "",
+                  'password1': "",
+                  'password2': ""}
