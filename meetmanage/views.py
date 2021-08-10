9b303070f02fd531bb463ee8ca769aa1ba01509d
Nicolas <n.ico15935746@gmail.com>
2021-08-10 03:42:04 -0500

    [Added] Views to signup, login and logout

--- meetmanage/views.py
+++ meetmanage/views.py
@@ -5,92 +5,92 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, CustomUserCreationForm
 from rest_framework.authtoken.models import Token
+from django.views.generic import CreateView, TemplateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
-    ''' Root page '''
-    context = {}
-    context['login'] = LoginForm()
-    context['signup'] = SignUpForm()
-    if request.user.is_authenticated:
-        return redirect("/panel")
-    return render(request, 'meetmanage/index.html', context)
+class Index(TemplateView):
+    ''' Index page. '''
+    template_name = 'meetmanage/index.html'
+
+    def get(self, request, *args, **kwargs):
+        ''' Show signup and login form. '''
+        if request.user.is_authenticated:
+            return redirect('/panel')
+
+        signup_form = CustomUserCreationForm
+        login_form = LoginForm
+        context = self.get_context_data(**kwargs)
+        context['signup_form'] = signup_form
+        context['login_form'] = login_form
+        return self.render_to_response(context)
 
 
-class Panel(View):
+class SignUp(CreateView):
+    ''' Allow users to signup '''
+    form_class = CustomUserCreationForm
+
+    def form_valid(self, form):
+        ''' If the form is valid, then: '''
+        form.save()
+        user = authenticate(
+            username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
+        login(self.request, user)
+        return redirect('/panel')
+
+    def form_invalid(self, form):
+        ''' If the form is invalid, then: '''
+        messages.error(self.request, form.errors)
+        return redirect("/")
+
+
+class Login(View):
+    ''' Allow login for users. '''
+
+    def post(self, request):
+        ''' Accept the data sent and login the user. '''
+        user = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=user, password=password)
+        if not user:
+            messages.error(self.request, "Invalid password or username")
+            return redirect("/")
+        login(self.request, user)
+        return redirect('/panel')
+
+
+class Panel(LoginRequiredMixin, View):
     '''
     Panel for authenticated users.
     As is no needed to use multiple views for login and signup, we can receive
     posts here, so user is always being redirected to panel after login/register.
     '''
+    login_url = '/'
     context = {}
 
     def get(self, request):
         ''' Panel to show the reservations. '''
-        if not request.user.is_authenticated:
-            return redirect("/")
-
         user = User.objects.filter(username=request.user).first()
         self.context['token'], created = Token.objects.get_or_create(user=user)
         return render(request, 'meetmanage/panel.html', self.context)
 
-    def post(self, request):
-        ''' When log in or sign up.'''
-        post = request.POST
 
-        # Basic validation to check if data sent is correct:
-        if 'password' not in post:
-            messages.error(request, "Insert a password.")
-            return redirect('/')
-        elif 'username' not in post:
-            messages.error(request, "Insert an user")
-            return redirect('/')
+class NewMeeting(LoginRequiredMixin, View):
+    ''' For users, can create new meetings. '''
+    login_url = '/'
 
-        if 'email' not in post:
-            login_form = LoginForm(post)
-            if login_form and login_form.is_valid():
-                login_data = login_form.data
-                username = login_data['username']
-                password = login_data['password']
+    def get(self, request):
+        ''' Show the html to create new meeting by using the api. '''
+        user = User.objects.filter(username=request.user).first()
+        return render(request, 'meetmanage/new.html')
 
-                user = authenticate(username='a', password='1234')
-                print(user)
-                if not user:  # If credentials wrong.
-                    messages.error(request, "Incorrect credentials.")
-                    return redirect('/')
 
-                login(request, user)
-                # Needs to be replaced for the panel html
-                self.context['token'], created = Token.objects.get_or_create(
-                    user=user)
-                return render(request, 'meetmanage/panel.html', self.context)
+class Logout(View):
+    """ Allow to logout for users. """
 
-        # Email exists so try to signup:
-        signup = SignUpForm(post)
-        if signup and signup.is_valid():
-            signup_data = signup.data
-            username = signup_data['username']
-            first_name = signup_data['first_name']
-            last_name = signup_data['last_name']
-            email = signup_data['email']
-            password = signup_data['password']
-
-            if User.objects.filter(username=username).first():
-                messages.error(request, "This username is already taken")
-                return redirect('/')
-            elif User.objects.filter(email=email).first():
-                messages.error(request, "This email is already taken")
-                return redirect('/')
-
-            user = User.objects.create_user(
-                username=username, email=email, first_name=first_name, last_name=last_name, password=password)
-            user.save()
-            login(request, user)
-
-            self.context['token'], created = Token.objects.get_or_create(
-                user=user)
-
-        # Needs to be replaced for the panel html
-        return render(request, 'meetmanage/panel.html', self.context)
+    def get(self, request):
+        ''' Logout an user '''
+        logout(request)
+        return redirect('/')
